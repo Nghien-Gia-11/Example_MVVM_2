a428fdfee8766f577131354de66cbf8e1c130928
Phung Gia Nghien <phungnghien03@gmail.com>
2024-07-23 10:22:09 +0700

    final game

--- app/src/main/java/com/example/example_mvvm_2/MainActivity.kt
+++ app/src/main/java/com/example/example_mvvm_2/MainActivity.kt
@@ -5,35 +5,29 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.indices
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.bumptech.glide.Glide
 import com.example.example_mvvm_2.Adapter.AnswerAdapter
 import com.example.example_mvvm_2.Adapter.SuggestAdapter
 import com.example.example_mvvm_2.Model.Question
 import com.example.example_mvvm_2.OnClickRecycler.OnClick
 import com.example.example_mvvm_2.ViewModel.MainViewModel
 import com.example.example_mvvm_2.databinding.ActivityMainBinding
-import com.example.example_mvvm_2.databinding.LayoutTextSuggestBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var listPic = mutableListOf<Question>()
     private val viewModel: MainViewModel by viewModels()
+
+    private var listPic = mutableListOf<Question>()
     private lateinit var answerAdapter: AnswerAdapter
     private lateinit var question: Question
     private lateinit var suggest: String
     private val answer = mutableListOf<String>()
     private lateinit var idPic: String
-    private var check = ""
-    private var score = 0
-    private var heart = 5
+
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,25 +42,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.imgBgNext.setOnClickListener {
-            randomQuestion()
-            answer.clear()
+            randomQuestion() // random câu hỏi
+            answer.clear() // clear đáp án trước khi vào câu hỏi mới
             setAdapterAnswer()
-            answerAdapter.setBackgroundChanged(1)
+            answerAdapter.setBackgroundChanged(1) // sửa lại màu nền của ô nhập đáp án là 1 nếu chưa nhập đủ đáp án
             answerAdapter.notifyDataSetChanged()
+            inVisibleNext()
         }
     }
 
     private fun randomQuestion() {
-        question = listPic.random()
-        suggest = question.answer
-        idPic = suggest
+        if (listPic.isNotEmpty()){
+            question = listPic.random()
+            listPic.remove(question)
+        }else{
+            Toast.makeText(this, "Bạn đã hoàn thành trò chơi !!", Toast.LENGTH_SHORT).show()
+        }
+        suggest = question.answer // lấy suggest là đáp án của câu hỏi
+        idPic = suggest // đáp án của câu hỏi
         Log.e("TAG", idPic)
         binding.imgQuestion.setImageResource(question.picture)
         setAdapterTextSuggest()
     }
 
     private fun setAdapterTextSuggest() {
-        textSuggest()
+        textSuggest() // trộn các ký tự của câu hỏi và các ký tự random khác
+        var check = ""
         binding.recyclerTextSuggest.apply {
             adapter = SuggestAdapter(suggest, object : OnClick {
                 @SuppressLint("NotifyDataSetChanged")
@@ -74,26 +75,31 @@ class MainActivity : AppCompatActivity() {
                     answer.add(suggest[pos].toString())
                     answerAdapter.notifyDataSetChanged()
                     if (answer.size == idPic.length) {
-                        check = ""
                         for (i in answer.indices) {
                             check += answer[i]
                         }
-                        Log.e("TAG", check)
                         if (check == idPic) {
-                            answerAdapter.setBackgroundChanged(2)
-                            score += 100
-                            binding.txtScore.text = score.toString()
-                            binding.imgBgNext.visibility = View.VISIBLE
-                            binding.txtNext.visibility = View.VISIBLE
+                            answerAdapter.setBackgroundChanged(2) // sửa lại màu nền của ô nhập đáp án là 2 nếu đúng
+                            viewModel.getScore()
+                            viewModel.score.observe(this@MainActivity) {
+                                binding.txtScore.text = it.toString()
+                            }
+                            visibleNext()
                             answerAdapter.notifyDataSetChanged()
-                            Toast.makeText(this@MainActivity, "Correct", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this@MainActivity, "Correct", Toast.LENGTH_SHORT).show()
                         } else {
-                            answerAdapter.setBackgroundChanged(3)
-                            heart -= 1
-                            binding.imgBgNext.visibility = View.VISIBLE
-                            binding.txtNext.visibility = View.VISIBLE
-                            binding.txtHeart.text = heart.toString()
+                            answerAdapter.setBackgroundChanged(3) // sửa lại màu nền của ô nhập đáp án là 3 nếu sai
+                            viewModel.getHeart()
+                            viewModel.heart.observe(this@MainActivity) {
+                                binding.txtHeart.text = it.toString()
+                                if (it == 0){
+                                    Toast.makeText(this@MainActivity, "bạn đã thua", Toast.LENGTH_LONG).show()
+                                    finish()
+                                }
+                            }
+                            visibleNext()
                             answerAdapter.notifyDataSetChanged()
+                            Toast.makeText(this@MainActivity, "Incorrect", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
@@ -121,9 +127,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setTextRandom(charter: Char): String {
-        val randomIndex = Random.nextInt(0, suggest.length + 1)
-        val first = suggest.substring(0, randomIndex)
+        val randomIndex =
+            Random.nextInt(0, suggest.length + 1) // lấy ra random 1 vị trí trong suggest
+        val first = suggest.substring(
+            0,
+            randomIndex
+        ) // tách suggest thành 2 chuỗi r chèn ký tự từ a -> z vào
         val second = suggest.substring(randomIndex)
         return first + charter + second
     }
+
+    private fun visibleNext() {
+        binding.imgBgNext.visibility = View.VISIBLE
+        binding.txtNext.visibility = View.VISIBLE
+    }
+
+    private fun inVisibleNext() {
+        binding.imgBgNext.visibility = View.INVISIBLE
+        binding.txtNext.visibility = View.INVISIBLE
+    }
+
 }
--- app/src/main/java/com/example/example_mvvm_2/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/example_mvvm_2/ViewModel/MainViewModel.kt
@@ -3,16 +3,27 @@ package com.example.example_mvvm_2.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.example_mvvm_2.Model.Question
 import com.example.example_mvvm_2.R
+import kotlinx.coroutines.launch
 
 class MainViewModel() : ViewModel() {
 
     private var _listPicture = MutableLiveData<List<Question>>()
     val listPicture : LiveData<List<Question>> get() = _listPicture
 
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int> get() = _score
+
+    private var _heart = MutableLiveData<Int>()
+    val heart : LiveData<Int> get() = _heart
+
+
     init {
         _listPicture.value = addImagePath()
+        _score.value = 0
+        _heart.value = 5
     }
 
     private fun addImagePath() : List<Question>{
@@ -25,8 +36,20 @@ class MainViewModel() : ViewModel() {
         listPic.add(Question(R.drawable.danhlua, "danhlua"))
         listPic.add(Question(R.drawable.danong, "danong"))
         listPic.add(Question(R.drawable.giandiep, "giandiep"))
-
         return listPic
     }
+
+    fun getScore(){
+        viewModelScope.launch {
+            _score.value = _score.value?.plus(100)
+        }
+    }
+
+    fun getHeart(){
+        viewModelScope.launch {
+            _heart.value = _heart.value?.minus(1)
+        }
+    }
+
 
 }
